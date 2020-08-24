c90ea42d8d23df27a5c6fe9b8d6ae414cd286652
Lucas China <lucashideyuki@gmail.com>
2020-08-24 19:46:38 -0300

    Add webpack configs

---
+++ webpack.common.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const publicPath = '/assets';
+const outputPath = path.join(__dirname, 'dist');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.bundle.js',
+        path: outputPath,
+        publicPath,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            // only enable hot in development
+                            hmr: process.env.NODE_ENV === 'development',
+                            // if hmr does not work, this is a forceful method.
+                            reloadAll: true,
+                        },
+                    },
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+            {
+                test: /\.txt$/i,
+                use: 'raw-loader',
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false,
+        }),
+        new HtmlWebpackPlugin({
+            // title: "Webpack 4 Starter",
+            template: './public/index.ejs',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false,
+            },
+        }),
+
+        new CopyPlugin({
+            patterns: [{ from: './public/assets', to: path.join(outputPath, publicPath) }],
+        }),
+
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
+};
